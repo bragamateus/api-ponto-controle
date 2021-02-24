53d5598a2e06e21d217c989ec05eca2430545a05
Mateus Braga <braga.mateus16@gmail.com>
2021-02-23 23:03:16 -0300

    remove query method

--- ponto-controle/src/main/java/io/github/bragamateus/pontocontrole/domain/repository/RelatorioRepository.java
+++ ponto-controle/src/main/java/io/github/bragamateus/pontocontrole/domain/repository/RelatorioRepository.java
@@ -4,10 +4,6 @@ import io.github.bragamateus.pontocontrole.domain.model.Relatorio;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
-
 @Repository
 public interface RelatorioRepository extends JpaRepository<Relatorio, Long> {
-    Optional<Relatorio> findByMes(String mes);
 }
