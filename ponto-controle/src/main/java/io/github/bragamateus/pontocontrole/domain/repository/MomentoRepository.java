ffd985ccc90fd65960615c6e5c182b79c0f349b0
Mateus Braga <braga.mateus16@gmail.com>
2021-02-23 22:28:36 -0300

    cria query para buscar data inicio e data fim

--- ponto-controle/src/main/java/io/github/bragamateus/pontocontrole/domain/repository/MomentoRepository.java
+++ ponto-controle/src/main/java/io/github/bragamateus/pontocontrole/domain/repository/MomentoRepository.java
@@ -2,9 +2,17 @@ package io.github.bragamateus.pontocontrole.domain.repository;
 
 import io.github.bragamateus.pontocontrole.domain.model.Momento;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Repository
 public interface MomentoRepository extends JpaRepository<Momento, Long> {
+
+    @Query("select m from Momento m where m.dataHora >= :inicio and m.dataHora <= :fim")
+    public List<Momento> findByPeriodo(@Param("inicio") LocalDateTime inicio, @Param("fim")LocalDateTime fim);
 
 }
