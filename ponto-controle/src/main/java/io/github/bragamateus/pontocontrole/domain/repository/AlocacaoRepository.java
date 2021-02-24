7e0f0f2aea229f740540a3e5989d05806b315c57
Mateus Braga <braga.mateus16@gmail.com>
2021-02-24 07:08:25 -0300

    cria query para obter alocacoes por periodo

--- ponto-controle/src/main/java/io/github/bragamateus/pontocontrole/domain/repository/AlocacaoRepository.java
+++ ponto-controle/src/main/java/io/github/bragamateus/pontocontrole/domain/repository/AlocacaoRepository.java
@@ -2,8 +2,16 @@ package io.github.bragamateus.pontocontrole.domain.repository;
 
 import io.github.bragamateus.pontocontrole.domain.model.Alocacao;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Repository
 public interface AlocacaoRepository extends JpaRepository<Alocacao, Long> {
+    @Query("select a from Alocacao a where a.dia >= :inicio and a.dia <= :fim")
+    public List<Alocacao> findAlocacaoByPeriodo(@Param("inicio") LocalDate inicio,
+                                                @Param("fim")LocalDate fim);
 }
