02d613cbccc0c71ecc2ffb899f44d13619d1aa98
Mateus Braga <braga.mateus16@gmail.com>
2021-02-24 07:45:50 -0300

    deleta repository nao usado

--- ponto-controle/src/main/java/io/github/bragamateus/pontocontrole/domain/repository/RelatorioRepository.java
+++
@@ -1,9 +0,0 @@
-package io.github.bragamateus.pontocontrole.domain.repository;
-
-import io.github.bragamateus.pontocontrole.domain.model.Relatorio;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RelatorioRepository extends JpaRepository<Relatorio, Long> {
-}
