5325b12fa07b8293dcddae87700c5d2b3e4e1fc7
Mateus Braga <braga.mateus16@gmail.com>
2021-02-23 21:52:00 -0300

    cria DTO para relatório

---
+++ ponto-controle/src/main/java/io/github/bragamateus/pontocontrole/domain/model/dto/RelatorioDTO.java
@@ -0,0 +1,12 @@
+package io.github.bragamateus.pontocontrole.domain.model.dto;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class RelatorioDTO {
+
+    private List<DiasTrabalhadosDTO> registros;
+
+}
